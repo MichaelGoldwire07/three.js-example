a014560def1ee140cfbfd097808d7a56da2c3d0a
MichaelGoldwire07 <michaelgoldwire07@gmail.com>
2021-09-23 20:45:23 -0500

    Mikey's three.js practice site

---
+++ three.js test/vite-project/main.js
@@ -0,0 +1,128 @@
+import './style.css'
+
+import * as THREE from 'three';
+
+import { OrbitControls }  from 'three/examples/jsm/controls/OrbitControls';
+import { TextureLoader } from 'three';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ(30);
+
+renderer.render( scene, camera );
+
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+const material = new THREE.MeshStandardMaterial( { color: 0xFF6347, } );
+const torus = new THREE.Mesh( geometry, material );
+scene.add(torus);
+
+const pointlight = new THREE.PointLight(0xffffff);
+pointlight.position.set(5,5,5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointlight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointlight);
+const gridHelper = new THREE.GridHelper(200,50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls( camera, renderer.domElement);
+
+
+function addStar()  {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial( { color: 0xffffff } )
+  const star = new THREE.Mesh( geometry, material );
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ) ); 
+
+  star.position.set(x, y, z,);
+  scene.add(star)
+
+}
+
+Array(200).fill().forEach(addStar)
+
+const spaceTexture = new THREE.TextureLoader().load('space background.jpg');
+scene.background = spaceTexture;
+
+
+
+
+renderer.render(scene, camera);
+
+
+
+function animate(){
+requestAnimationFrame(animate);
+
+ 
+//now changing the position of the donut
+torus.rotation.x += 0.01;
+torus.rotation.y += 0.005;
+torus.rotation.z += 0.005;
+
+controls.update();
+
+renderer.render(scene, camera);
+
+}
+
+animate();
+
+
+
+//avatar
+
+const jeffTexture = new TextureLoader().load('jeff.png');
+
+const jeff = new THREE.Mesh(
+  new THREE. BoxGeometry(3,3,3),
+  new THREE.MeshBasicMaterial( { map:jeffTexture } )
+);
+
+scene.add(jeff);
+
+
+//moon
+
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+new THREE.MeshStandardMaterial( {
+    map: moonTexture
+
+} )
+);
+
+scene.add(moon);
+
+
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+
+
+function moveCamera() {
+
+const t = document.bodygetBoundingClientRect().top;
+moon.rotation.x += 0.05;
+moon.rotation.y += 0.075;
+moon.rotation.z += 0.05;
+
+camera.position.z = t * -0.01;
+camera.position.x = t * -0.0002;
+camera.position.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera
